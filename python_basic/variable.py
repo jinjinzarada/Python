70631a017c98829548a75fd7284305198255019e
myusername <jinjinzara774@gmail.com>
2022-04-01 21:52:26 +0900

    업무 자동화를 위한 파이썬 기초
    파이썬 변수

---
+++ python_basic/variable.py
@@ -0,0 +1,46 @@
+a = 100
+b = 200
+a,b = 100, 200
+a,b=b,a+b
+
+print(a,b)
+print(a)
+del b
+print(b)
+#######################################
+text = "Hello World!"
+text = "Welcome to Python!"
+print((text + "\n")*10)
+
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+########################################
+
+name = "최은진"
+phone_number = "010-2222-3333"
+address ="강원도 원주시"
+
+print(name)
+print(phone_number)
+print(address)
+########################################
+apple = "사과"
+apples = "사과들"
+apple3 = "사과3개"
+app3le = "사3과"
+red_apple = "빨간 사과"
+very_delocious_red_apple = "아주 맛있는 빨간 사과"
+
+# 잘못된 예
+# 3apple = "3개의 사과"
+# red apple = "빨간 사과"
+# apple! ="사과!"
+# apple$ "사과$"
