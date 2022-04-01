c98dba92583d348215dc104fecde6ac1bde94385
myusername <jinjinzara774@gmail.com>
2022-04-02 00:41:47 +0900

    업무 자동화를 위한 파이썬 기초
    파이썬 인덱스

---
+++ python_basic/index.py
@@ -0,0 +1,44 @@
+# 문자열 인덱싱
+# 지정된 문자 뽑아내기
+# index
+# 01234 양수는 0부터
+# -5-4-3-2-1 음수는 -1부터
+# 공백도 인덱스 포함
+
+from turtle import right
+
+
+print("index"[0])
+print("index"[1])
+print("index"[2])
+print("index"[3])
+print("index"[4])
+
+print("index"[-5])
+print("index"[-4])
+print("index"[-3])
+print("index"[-2])
+print("index"[-1])
+
+#String index
+
+str_="String index"
+
+print(str_[0])
+print(str_[1])
+print(str_[2])
+print(str_[3])
+print(str_[4])
+print(str_[5])
+print(str_[6])
+print(str_[7])
+print(str_[8])
+print(str_[9])
+print(str_[10])
+print(str_[11])
+# print(str_[12]) indexerror 넘어서면 안된다.
+
+print(str_[-1])
+print(str_[-2])
+print(str_[-3])
+print(str_[-4])
