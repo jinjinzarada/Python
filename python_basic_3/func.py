3b95eeb48de3b88e2ba6fb8324847ec70d07f4f0
myusername <jinjinzara774@gmail.com>
2022-05-18 12:35:35 +0900

    업무 자동화를 위한 파이썬 기초 - 내장함수
    파이썬 - 내장함수

---
+++ python_basic_3/func.py
@@ -0,0 +1,25 @@
+# 파이썬의 내장함수
+# print() 문자열로 출력
+# input() 입력을 문자열로 변환
+# 함수 정의하기(define)
+# def 함수이름():
+#   수행할 코드
+
+# def menu():
+#     print("오늘의 메뉴")
+#     print("오늘 점심메뉴는 제육볶음입니다.")
+#     print("내일의 메뉴")
+#     print("내일의 점심메뉴는 돈까스입니다.")
+
+# menu()
+
+# print("사용할 데이터")
+# input("데이터")
+
+# def add(text1, text2):
+#     print(text1+text2)
+
+#     text=text1 + text2
+#     return text
+
+# print(add("홍","길동"))
