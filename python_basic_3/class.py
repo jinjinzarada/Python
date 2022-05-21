fd76d4b8e4b3a633d6461b9569679da4b19f0a4e
myusername <jinjinzara774@gmail.com>
2022-05-21 22:30:37 +0900

    업무 자동화를 위한 파이썬 기초 - 참조연산자
    파이썬 - class 참조연산자

---
+++ python_basic_3/class.py
@@ -0,0 +1,15 @@
+class BreadMold:
+    category =  "크림빵"
+    def make_bread(self):
+        print("빵을 만들어 냅니다.")
+
+bread = BreadMold()
+
+bread.make_bread()
+
+# bread.price = 3000
+# bread_choco.category = "초코크림빵"
+
+# # 참조연산자.format()
+# print("{}의 가격은 {}입니다.".format(bread.category, bread.price))
+# print(bread_choco.category, bread_choch.price)
